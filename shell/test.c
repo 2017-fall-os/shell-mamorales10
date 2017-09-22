dfd09f30fc0997c63d57c465a3523dd1b6797d36
Max Morales <mamorales10@miners.utep.edu>
2017-09-22 11:48:19 -0600

    Added working strLength and strConcat functions

--- shell/test.c
+++ shell/test.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include "mytoc.h"
 
 /* Prints the tokens in the vector */
@@ -12,6 +13,36 @@ void printTokens(char **vect){
     tempTok++;
   }
   
+}
+
+int strLength(char *str){
+
+  char *temp = str;
+  char i;
+  int len = 0;
+  for(i = 0; temp[i] || temp[i] != '\0'; i++){
+    len++;
+  }
+  return len;
+}
+
+char *strConcat(char *src, char *dst){
+
+  char srcLen = strLength(src);
+  char dstLen = strLength(dst);
+  char *finalStr = (char *)malloc(sizeof(char)*(srcLen + dstLen + 1));
+
+  char i;
+  for(i = 0; i < (srcLen+dstLen); i++){
+    if(i < srcLen){
+      finalStr[i] = src[i];
+    }
+    else{
+      finalStr[i] = dst[i-srcLen];
+    }
+  }
+  finalStr[i] = 0;
+  return finalStr;
 }
 
 /* Checks if input equals PATH*/
@@ -72,10 +103,6 @@ int main(int argc, char** argv, char** envp){
   free(testVect);
   printTokens(pathVect);        /*DELETE THIS*/
   
-
-
-
-  
   if(write(1, "$ ", 2) != 2){
     write(2, "There was an error.\n", 20);
     return -1;
@@ -85,11 +112,26 @@ int main(int argc, char** argv, char** envp){
   while(!isExit(buff)){
   
     vect = mytoc((char *)buff, ' ');
-
     printTokens(vect);        /*DELETE THIS*/
+    /*
+    int pid = fork();
+    if(pid < 0){
+      printf("Failed to create new process\n");
+    }
+    else if(pid == 0){
+      int execVal = execve(vect[0], vect, envp);
 
-    
+      for(i=0; execVal != 0; i++){
+	
+	
+      }
 
+    }
+    else{
+      int retPID = wait(NULL);
+      printf("Child Process returned");
+    }
+    */
     free(vect); //Frees token vector
     if(write(1, "$ ", 2) != 2){
       write(2, "There was an error.\n", 20);
